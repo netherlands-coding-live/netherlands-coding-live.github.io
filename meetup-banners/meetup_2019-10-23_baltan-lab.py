c3fbe79506f079b7de3f46b306ee0365ca89789c
tmhglnd <timohoogland@gmail.com>
2019-10-16 12:45:37 +0200

    added banner images for upcoming meetups

---
+++ meetup-banners/meetup_2019-10-23_baltan-lab.py
@@ -0,0 +1,11 @@
+
+#===================================
+NL_CL Monthly Meetup
+#===================================
+
+@ Baltan Laboratories
+	During the Dutch Design Week
+	NATLAB Eindhoven
+	-> 18.00 - 20.00
+
+#===================================
